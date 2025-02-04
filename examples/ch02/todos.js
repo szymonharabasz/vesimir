846fe1ecf1741ae66ea692412b87c0811c185514
Szymon Harabasz <szymon.harabasz@gmail.com>
2025-02-04 16:33:53 +0100

    Implemented adding, removing and updating todos

--- examples/ch02/todos.js
+++ examples/ch02/todos.js
@@ -24,9 +24,69 @@ addTodoButton.addEventListener('click', () => {
 })
 
 function renderTodoInReadMode(todo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.textContent = todo;
+    span.addEventListener('dblclick', () => {
+        const idx = todos.indexOf(todo);
+        todosList.replaceChild(renderTodoInEditMode(todo), todosList.childNodes[idx]);
 
+    });
+    li.append(span);
+
+    const button = document.createElement("button");
+    button.textContent = 'Done';
+    button.addEventListener('click', () => {
+        const idx = todos.indexOf(todo);
+        removeTodo(idx);
+    });
+    li.append(button);
+    return li;
 }
 
-function addTodo() {
+function renderTodoInEditMode(todo) {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = todo;
+    li.append(input);
 
+    const saveBtn = document.createElement("button");
+    saveBtn.textContent = 'Save';
+    saveBtn.addEventListener("click", () => {
+        const idx = todos.indexOf(todo);
+        updateTodo(idx, input.value);
+    });
+    li.append(saveBtn);
+
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Cancel";
+    cancelBtn.addEventListener("click", () => {
+        const idx = todos.indexOf(todo);
+        todosList.replaceChild(renderTodoInReadMode(todo), todosList.childNodes[idx]);
+    });
+    li.append(cancelBtn);
+
+    return li;
+}
+
+
+function addTodo() {
+    const description = addTodoInput.value;
+    todos.push(description);
+    const todo = renderTodoInReadMode(description);
+    todosList.append(todo);
+    addTodoInput.value = "";
+    addTodoButton.disabled = true;
+}
+
+function removeTodo(index) {
+    todos.splice(index, 1);
+    todosList.childNodes[index].remove();
+}
+
+function updateTodo(index, description) {
+    todos[index] = description;
+    const todo = renderTodoInReadMode(description);
+    todosList.replaceChild(todo, todosList.childNodes[index]);
 }
