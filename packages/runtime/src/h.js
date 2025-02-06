454eb5044c59414773996523c0fab65e47206787
Szymon Harabasz <szymon.harabasz@gmail.com>
2025-02-06 07:57:06 +0100

    Added function to create element and text nodes of virtual DOM

---
+++ packages/runtime/src/h.js
@@ -0,0 +1,26 @@
+import { withoutNulls } from "./utils/arrays";
+
+export const DOM_TYPES = {
+  TEXT: "text",
+  ELEMENT: "element",
+  FRAGMENT: "fragment",
+};
+
+export function h(tag, props = {}, childern = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(childern)),
+    type: DOM_TYPES.ELEMENT,
+  };
+}
+
+function mapTextNodes(children) {
+  return children.map((child) =>
+    typeof child === "string" ? hString(child) : child
+  );
+}
+
+export function hString(str) {
+  return { value: str, type: DOM_TYPES.TEXT };
+}
